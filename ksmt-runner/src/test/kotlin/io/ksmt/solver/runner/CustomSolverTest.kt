2d1d6689ec3b07929a5ed7f585b27161dcd0a47e
Pavel Balay <pavel.balai@gmail.com>
2024-05-15 15:11:51 +0300

    Disable ksmt runner tests

--- ksmt-runner/src/test/kotlin/io/ksmt/solver/runner/CustomSolverTest.kt
+++ ksmt-runner/src/test/kotlin/io/ksmt/solver/runner/CustomSolverTest.kt
@@ -10,10 +10,12 @@ import io.ksmt.solver.KSolverUniversalConfigurationBuilder
 import io.ksmt.solver.KTheory
 import io.ksmt.solver.model.KModelImpl
 import io.ksmt.sort.KBoolSort
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.time.Duration
 
+@Ignore
 class CustomSolverTest {
 
     @Test
